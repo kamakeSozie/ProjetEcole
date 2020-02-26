1d01fed17da5bfaf7f580921a8a91a45efd0817b
Sozie <kamakeyaovi@gmail.com>
2020-02-26 11:58:41 +0000

    first commit

---
+++ SchoolManagementSystem.Backend/SchoolManagementSystem.Services/Core/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagementSystem.Services.Core.Repositories
+{
+    public interface IRepository
+    {
+    }
+}
